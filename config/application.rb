12b7f0cdbf76adda564c2dd97999ebcc519152c8
Ehbraheem <Ehbraheem@gmail.com>
2016-06-14 15:39:06 +0100

    Assignment 5 for week 5, where the post html code and comment html code are all edited

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,18 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+# Created middleware for getting the amount of time it take to return a response to the request
+require_relative '../lib/timer'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Blog
   class Application < Rails::Application
+      
+      # Adding the middleware to our app 
+      config.middleware.insert_before(Rack::Sendfile, Timer)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
